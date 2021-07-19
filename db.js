a3f550a1db818af18771431a40a10ae5e11469c7
NeelParihar <neel@terminusdb.com>
2021-07-19 21:39:46 +0530

    added db connector
    Signed-off-by: NeelParihar <neel@terminusdb.com>

---
+++ db.js
@@ -0,0 +1,21 @@
+const { WOQLClient } = require("@terminusdb/terminusdb-client");
+
+const server = "http://localhost:6363";
+
+const client = new WOQLClient(server, {
+  user: "admin",
+  organization: "admin",
+  key: "root",
+});
+
+
+const connectDB = async () => {
+  try {
+    await client.connect();
+    return client;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = connectDB;
