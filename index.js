3c9f425651664c801354bc624fe38233052a881c
NeelParihar <neel@terminusdb.com>
2021-08-03 15:06:00 +0530

    added index script
    Signed-off-by: NeelParihar <neel@terminusdb.com>

--- index.js
+++ index.js
@@ -1,180 +1,132 @@
-const connectDB = require("./db");
-const readCsv = require("./loadAndParseCSV");
-const util = require("./util");
-
-// Schemas for documents
-const json_schema = [
-  {
-    "@type": "Class",
-    "@id": "Country",
-    "@key": {
-      "@type": "Lexical",
-      "@fields": ["Name"],
-    },
-    Name: "xsd:string",
-  },
-  {
-    "@type": "Class",
-    "@id": "BootcampInfo",
-    "@subdocument": [],
-    "@key": {
-      "@type": "ValueHash",
-      "@fields": ["BootcampName"],
-    },
-    BootcampName: "xsd:string",
-    AttendedBootcamp: "xsd:string",
-    BootcampFinish: "xsd:string",
-    BootcampLoanYesNo: "xsd:string",
-    BootcampRecommend: "xsd:string",
-  },
-  {
-    "@type": "Class",
-    "@id": "PersonalInfo",
-    "@subdocument": [],
-    "@key": {
-      "@type": "ValueHash",
-      "@fields": ["Gender"],
-    },
-    Gender: "xsd:string",
-    HasChildren: "xsd:string",
-    HasDebt: "xsd:string",
-    HasFinancialDependents: "xsd:string",
-    HasHomeMortgage: "xsd:string",
-    HasServedInMilitary: "xsd:string",
-    HasStudentDebt: "xsd:string",
-    HomeMortgageOwe: "xsd:string",
-    Income: "xsd:string",
-    IsEthnicMinority: "xsd:string",
-    MaritalStatus: "xsd:string",
-  },
-  {
-    "@type": "Class",
-    "@id": "JobInfo",
-    "@subdocument": [],
-    "@key": {
-      "@type": "ValueHash",
-      "@fields": ["JobPref"],
-    },
-    IsSoftwareDev: "xsd:string",
-    IsUnderEmployed: "xsd:string",
-    JobApplyWhen: "xsd:string",
-    JobInterest: { "@type": "Set", "@class": "Job" },
-    JobPref: "xsd:string",
-    JobRelocateYesNo: "xsd:string",
-    JobRoleInterest: "xsd:string",
-  },
-  {
-    "@type": "Class",
-    "@id": "Coder",
-    "@base": "Coder_",
-    "@key": {
-      "@type": "Lexical",
-      "@fields": ["Id"],
-    },
-    Id: "xsd:string",
-    Age: "xsd:string",
-    BootcampInfo: "BootcampInfo",
-    EventsAttended: { "@type": "Set", "@class": "Event" },
-    CommuteTime: "xsd:string",
-    CityPopulation: "xsd:string",
-    CountryCitizen: "Country",
-    CountryLive: "Country",
-    EmploymentField: "xsd:string",
-    EmploymentStatus: "xsd:string",
-    ExpectedEarning: "xsd:string",
-    FinanciallySupporting: "xsd:string",
-    FirstDevJob: "xsd:string",
-    PersonalInfo: "PersonalInfo",
-    JobInfo: "JobInfo",
-    HoursLearning: "xsd:string",
-    LanguageAtHome: "xsd:string",
-    MoneyForLearning: "xsd:string",
-    MonthsProgramming: "xsd:string",
-    ListensToPodcast: { "@type": "Set", "@class": "Podcast" },
-    UsesResource: { "@type": "Set", "@class": "Resource" },
-    SchoolDegree: "xsd:string",
-    SchoolMajor: "xsd:string",
-    StudentDebtOwe: "xsd:string",
-    Channels: { "@type": "Set", "@class": "Channel" },
-  },
-];
+const ejs = require("ejs");
+const fs = require("fs");
+const connectDB = require("./src/db");
+const queries = require("./src/queries");
+const createDocuments = require("./src/createDocuments");
 
 const runScript = async () => {
   try {
     // connect to db
     const client = await connectDB();
     const CoderDB = "NewCoder_test017";
-    const instanceData = await readCsv();
 
-    // insert Job Enum schema at start
-    json_schema.unshift({
-      "@type": "Enum",
-      "@id": "Job",
-      "@value": [...util.getJobs(Object.keys(instanceData[0])), "NA"],
-    });
-
-    // insert Podcast Enum schema at start
-    json_schema.unshift({
-      "@type": "Enum",
-      "@id": "Podcast",
-      "@value": [...util.getPodcasts(Object.keys(instanceData[0])), "NA"],
-    });
-
-    // insert Resource Enum schema at start
-    json_schema.unshift({
-      "@type": "Enum",
-      "@id": "Resource",
-      "@value": [...util.getResources(Object.keys(instanceData[0])), "NA"],
-    });
-
-    // insert Channel Enum schema at start
-    json_schema.unshift({
-      "@type": "Enum",
-      "@id": "Channel",
-      "@value": [...util.getChannels(Object.keys(instanceData[0])), "NA"],
-    });
-
-    // insert Event Enum schema at start
-    json_schema.unshift({
-      "@type": "Enum",
-      "@id": "Event",
-      "@value": [...util.getEvents(Object.keys(instanceData[0])), "NA"],
-    });
-
-    await client.createDatabase(CoderDB, {
-      label: "New Coder Survey Database",
-      comment: "Created new coder survey database",
-    });
+    // await client.createDatabase(CoderDB, {
+    //   label: "New Coder Survey Database",
+    //   comment: "Created new coder survey database",
+    // });
 
     client.db(CoderDB);
-    await client.addDocument(json_schema, { graph_type: "schema" });
-    console.log("Created Schema successfully");
 
-    // insert Country documents
-    const countryInstances = util
-      .getDistinctCountries(instanceData)
-      .map((country) => {
-        return {
-          "@type": "Country",
-          Name: country,
-        };
-      });
-    await client.addDocument(countryInstances);
+    // await createDocuments.createSchema(client);
 
-    // get docuemnts to insert into database
-    const coderInstances = util.getInstances(instanceData);
+    // await createDocuments.insertDocumentInstances(client);
 
-    // Insert list of documents in chunks 
-    let i, j, chunkRows, chunk = 2000;
-    for (i = 0, j = coderInstances.length; i < j; i += chunk) {
-      chunkRows = coderInstances.slice(i, i + chunk);
-      await client.addDocument(chunkRows);
-      console.log(chunkRows.length + " Instances Inserted");
-    }
+    let resultMostPopularJobs = await queries.getMostPopularJobs(client);
+    resultMostPopularJobs = resultMostPopularJobs.map((jobs) => {
+      return [jobs.JobInterest.slice(23), jobs.JobInterestGroupCount["@value"]];
+    });
 
-    const documents = await client.getDocument({ as_list: true });
-    console.log("Successfully Inserted "+documents.length+" instances");
-  
+    let resultMostPopularResource = await queries.getMostPopularResource(
+      client
+    );
+    resultMostPopularResource = resultMostPopularResource.map((resource) => {
+      return [
+        resource.UsesResource.slice(25),
+        resource.UsesResource_group.length,
+      ];
+    });
+
+    let resultMostPopularChannel = await queries.getMostPopularChannel(client);
+    resultMostPopularChannel = resultMostPopularChannel.map((Channel) => {
+      return [Channel.Channels.slice(23), Channel.Channels_group.length];
+    });
+
+    let resultMostPopularPodcast = await queries.getMostPopularPodcast(client);
+    resultMostPopularPodcast = resultMostPopularPodcast.map((Podcast) => {
+      return [
+        Podcast.ListensToPodcast.slice(23),
+        Podcast.ListensToPodcast_group.length,
+      ];
+    });
+
+    let resultMostPopularCodeEvent = await queries.getMostPopularCodeEvent(
+      client
+    );
+    resultMostPopularCodeEvent = resultMostPopularCodeEvent.map((CodeEvent) => {
+      return [
+        CodeEvent.EventsAttended.slice(23),
+        CodeEvent.EventsAttended_group.length,
+      ];
+    });
+
+    let resultCodersBetween2030Unemployed =
+      await queries.getCodersBetween2030Unemployed(client);
+
+    let resultMostUnderEmployedCountry =
+      await queries.getMostUnderEmployedCountry(client);
+    resultMostUnderEmployedCountry = resultMostUnderEmployedCountry.map(
+      (Country) => {
+        if (
+          Country.CountryLive.slice(8).split("%20").join(" ") ===
+          "Cote D'Ivoire"
+        ) {
+          return ["Cote D Ivoire", Country.CountryLiveGroupCount["@value"]];
+        }
+        return [
+          Country.CountryLive.slice(8).split("%20").join(" ").toString(),
+          Country.CountryLiveGroupCount["@value"],
+        ];
+      }
+    );
+
+    let resultCountOfCodersNotInComputerField =
+      await queries.getCountOfCodersNotInComputerField(client);
+    await queries.getAverageIncomeOfCountries(client);
+
+    console.log("Execution of queries successful, rendering report file..");
+
+    // Create a HTML report to visualize results
+    ejs.renderFile(
+      "./resultTemplate.ejs",
+      {
+        data: {
+          MostPopularJobs: [
+            ["Jobs", "Count of Coders"],
+            ...resultMostPopularJobs,
+          ],
+          MostPopularResources: [
+            ["Resources", "Count of Coders"],
+            ...resultMostPopularResource,
+          ],
+          MostPopularChannel: [
+            ["Channels", "Count of Coders"],
+            ...resultMostPopularChannel,
+          ],
+          MostPopularPodcast: [
+            ["Podcasts", "Count of Coders"],
+            ...resultMostPopularPodcast,
+          ],
+          MostPopularCodeEvent: [
+            ["Code Events", "Count of Coders"],
+            ...resultMostPopularCodeEvent,
+          ],
+          CodersBetween2030Unemployed:
+            resultCodersBetween2030Unemployed[0].CountOfUnemployedCoders[
+              "@value"
+            ],
+          MostUnderEmployedCountry: [
+            ["Country", "Count of UnderEmployed Coders"],
+            ...resultMostUnderEmployedCountry,
+          ],
+          CountOfCodersNotInComputerField:
+            resultCountOfCodersNotInComputerField[0].CountOfCoders["@value"],
+        },
+      },
+      function (err, str) {
+        // str => Rendered HTML string
+        fs.writeFileSync("./result.html", str, "utf8");
+      }
+    );
   } catch (error) {
     console.error(error);
   }
